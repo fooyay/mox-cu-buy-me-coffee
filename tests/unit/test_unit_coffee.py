f726fe65222ceef1b63dc0b60a82afc649859cb3
Sean Coates <sean.coates+fooyay@gmail.com>
2026-01-21 16:04:00 -0500

    test withdraw and pranking user

--- tests/unit/test_unit_coffee.py
+++ tests/unit/test_unit_coffee.py
@@ -1,6 +1,9 @@
 from eth_utils import to_wei
 import boa
 
+SEND_VALUE = to_wei(1, "ether")
+RANDOM_USER = boa.env.generate_address("non-owner")
+
 
 def test_price_feed_is_correct(coffee, eth_usd_price_feed):
     assert coffee.PRICE_FEED() == eth_usd_price_feed.address
@@ -19,3 +22,29 @@ def test_fund_fails_with_no_eth(coffee):
 def test_fund_fails_with_insufficient_eth(coffee):
     with boa.reverts("You must spend more ETH!"):
         coffee.fund(value=to_wei(0.001, "ether"))
+
+
+def test_fund_with_money(coffee, default_account):
+    boa.env.set_balance(default_account.address, SEND_VALUE)
+    coffee.fund(value=SEND_VALUE)
+    funder = coffee.funders(0)
+    assert funder == default_account.address
+    assert coffee.funder_to_amount_funded(funder) == SEND_VALUE
+
+
+def test_non_owner_cannot_withdraw(coffee, default_account):
+    boa.env.set_balance(default_account.address, SEND_VALUE)
+    coffee.fund(value=SEND_VALUE)
+
+    with boa.env.prank(RANDOM_USER):
+        with boa.reverts("Not the contract owner!"):
+            coffee.withdraw()
+
+
+def test_owner_can_withdraw(coffee):
+    boa.env.set_balance(coffee.OWNER(), SEND_VALUE)
+    with boa.env.prank(coffee.OWNER()):
+        coffee.fund(value=SEND_VALUE)
+        coffee.withdraw()
+
+    assert boa.env.get_balance(coffee.address) == 0
