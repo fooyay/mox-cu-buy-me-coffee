7f2851ecc6ca2354d89030a84cbffc490b0d5ad9
Sean Coates <sean.coates+fooyay@gmail.com>
2026-01-21 15:14:39 -0500

    add default account fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,13 @@
 import pytest
 from script.deploy import deploy_coffee
 from script.deploy_mocks import deploy_price_feed
+from moccasin.config import get_active_network  # type: ignore
+
+
+@pytest.fixture(scope="session")
+def default_account():
+    active_network = get_active_network()
+    return active_network.get_default_account()
 
 
 @pytest.fixture(scope="session")
--- tests/unit/test_unit_coffee.py
+++ tests/unit/test_unit_coffee.py
@@ -5,5 +5,6 @@ def test_price_feed_is_correct(coffee, eth_usd_price_feed):
     assert coffee.PRICE_FEED() == eth_usd_price_feed.address
 
 
-def test_starting_values(coffee):
+def test_starting_values(coffee, default_account):
     assert coffee.MINIMUM_USD() == to_wei(5, "ether")
+    assert coffee.OWNER() == default_account.address
