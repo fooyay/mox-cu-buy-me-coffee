cd5910155f76b00e2e370132aeba0e7e95a26aa3
Sean Coates <sean.coates+fooyay@gmail.com>
2026-01-21 15:26:35 -0500

    test revert if no eth sent

--- tests/unit/test_unit_coffee.py
+++ tests/unit/test_unit_coffee.py
@@ -1,4 +1,5 @@
 from eth_utils import to_wei
+import boa
 
 
 def test_price_feed_is_correct(coffee, eth_usd_price_feed):
@@ -8,3 +9,8 @@ def test_price_feed_is_correct(coffee, eth_usd_price_feed):
 def test_starting_values(coffee, default_account):
     assert coffee.MINIMUM_USD() == to_wei(5, "ether")
     assert coffee.OWNER() == default_account.address
+
+
+def test_fund_fails_without_enough_eth(coffee):
+    with boa.reverts("You must spend more ETH!"):
+        coffee.fund()
