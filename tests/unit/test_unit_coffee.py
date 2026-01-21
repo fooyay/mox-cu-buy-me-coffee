f4a1c786a9a9b2f0bfe464ad686f3a4fb5e13ad4
Sean Coates <sean.coates+fooyay@gmail.com>
2026-01-21 15:27:42 -0500

    test revert if insufficient eth sent

--- tests/unit/test_unit_coffee.py
+++ tests/unit/test_unit_coffee.py
@@ -11,6 +11,11 @@ def test_starting_values(coffee, default_account):
     assert coffee.OWNER() == default_account.address
 
 
-def test_fund_fails_without_enough_eth(coffee):
+def test_fund_fails_with_no_eth(coffee):
     with boa.reverts("You must spend more ETH!"):
         coffee.fund()
+
+
+def test_fund_fails_with_insufficient_eth(coffee):
+    with boa.reverts("You must spend more ETH!"):
+        coffee.fund(value=to_wei(0.001, "ether"))
