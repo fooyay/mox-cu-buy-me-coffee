996f152679bce6a2ec90c4cd02b4c48f285783b7
Sean Coates <sean.coates+fooyay@gmail.com>
2026-01-21 15:07:42 -0500

    initial basic tests

---
+++ tests/unit/test_unit_coffee.py
@@ -0,0 +1,9 @@
+from eth_utils import to_wei
+
+
+def test_price_feed_is_correct(coffee, eth_usd_price_feed):
+    assert coffee.PRICE_FEED() == eth_usd_price_feed.address
+
+
+def test_starting_values(coffee):
+    assert coffee.MINIMUM_USD() == to_wei(5, "ether")
