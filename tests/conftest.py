418fe02ae85fbcdc1a6ed6aaec4b9060de3de165
Sean Coates <sean.coates+fooyay@gmail.com>
2026-01-22 09:12:32 -0500

    make a fixture for a funded contract

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,10 @@ import pytest
 from script.deploy import deploy_coffee
 from script.deploy_mocks import deploy_price_feed
 from moccasin.config import get_active_network  # type: ignore
+from eth_utils import to_wei
+import boa  # type: ignore
+
+SEND_VALUE = to_wei(1, "ether")
 
 
 @pytest.fixture(scope="session")
@@ -18,3 +22,11 @@ def eth_usd_price_feed():
 @pytest.fixture(scope="function")
 def coffee(eth_usd_price_feed):
     return deploy_coffee(eth_usd_price_feed)
+
+
+@pytest.fixture(scope="function")
+def coffee_funded(coffee, default_account):
+    boa.env.set_balance(default_account.address, SEND_VALUE)
+    with boa.env.prank(default_account.address):
+        coffee.fund(value=SEND_VALUE)
+    return coffee
--- tests/unit/test_unit_coffee.py
+++ tests/unit/test_unit_coffee.py
@@ -1,7 +1,7 @@
 from eth_utils import to_wei
 import boa  # type: ignore
+from tests.conftest import SEND_VALUE
 
-SEND_VALUE = to_wei(1, "ether")
 RANDOM_USER = boa.env.generate_address("non-owner")
 
 
@@ -32,22 +32,17 @@ def test_fund_with_money(coffee, default_account):
     assert coffee.funder_to_amount_funded(funder) == SEND_VALUE
 
 
-def test_non_owner_cannot_withdraw(coffee, default_account):
-    boa.env.set_balance(default_account.address, SEND_VALUE)
-    coffee.fund(value=SEND_VALUE)
-
+def test_non_owner_cannot_withdraw(coffee_funded, default_account):
     with boa.env.prank(RANDOM_USER):
         with boa.reverts("Not the contract owner!"):
-            coffee.withdraw()
+            coffee_funded.withdraw()
 
 
-def test_owner_can_withdraw(coffee):
-    boa.env.set_balance(coffee.OWNER(), SEND_VALUE)
-    with boa.env.prank(coffee.OWNER()):
-        coffee.fund(value=SEND_VALUE)
-        coffee.withdraw()
+def test_owner_can_withdraw(coffee_funded):
+    with boa.env.prank(coffee_funded.OWNER()):
+        coffee_funded.withdraw()
 
-    assert boa.env.get_balance(coffee.address) == 0
+    assert boa.env.get_balance(coffee_funded.address) == 0
 
 
 def test_multiple_funders_and_withdraw(coffee):
